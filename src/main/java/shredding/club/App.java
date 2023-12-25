7374824cd41a1c4dc71e8dd35db8baae1fb47571
jqckp <porterjp1@appstate.edu>
2023-12-25 17:41:03 -0500

    Add source code

---
+++ src/main/java/shredding/club/App.java
@@ -0,0 +1,45 @@
+package shredding.club;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+    public static void main( String[] args ) 
+    {
+        System.out.println( "Hello World!" );
+
+        
+
+        try {
+            launch(args);
+        } catch (Exception e) {
+            
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        AnchorPane root = new AnchorPane();
+
+        Scene scene = new Scene(root, 300, 300);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.setTitle("TESTING");
+
+        primaryStage.show();
+
+        
+
+    }
+}
