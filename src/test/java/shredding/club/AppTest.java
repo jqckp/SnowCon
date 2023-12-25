8af7f58cb05c793021eb7fb5b95da9badc43bc9d
jqckp <porterjp1@appstate.edu>
2023-12-25 18:16:30 -0500

    Remove default archetype tests

--- src/test/java/shredding/club/AppTest.java
+++ src/test/java/shredding/club/AppTest.java
@@ -1,38 +1,21 @@
 package shredding.club;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.jupiter.api.Test;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+
+    @Test
+    public void printWorking()
     {
-        super( testName );
+        System.out.println("test is working");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
+    
+
