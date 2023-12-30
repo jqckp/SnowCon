72fc01340b1e72c3d66628dc0cf7b47f54706d9f
jqckp <porterjp1@appstate.edu>
2023-12-30 13:24:32 -0500

    Write simple webscraping feature as trial

--- src/main/java/shredding/club/snowcon/Main.java
+++ src/main/java/shredding/club/snowcon/Main.java
@@ -1,10 +1,15 @@
 package shredding.club.snowcon;
 
+import shredding.club.snowcon.model.AppSkiMountain;
 import shredding.club.snowcon.view.GraphicalInterface;
 
 public class Main {
     
     public static void main(String[] args) {
-        GraphicalInterface.launchGUI(args);
+        //GraphicalInterface.launchGUI(args);
+
+        AppSkiMountain appSki = new AppSkiMountain();
+
+        appSki.collectData();
     }
 }
--- src/main/java/shredding/club/snowcon/model/AppSkiMountain.java
+++ src/main/java/shredding/club/snowcon/model/AppSkiMountain.java
@@ -1,6 +1,59 @@
 package shredding.club.snowcon.model;
 
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AppSkiMountain 
 {
+
+    private WebDriver driver;
     
+    private String url = "https://appskimtn.com/slope-report";
+
+    public void collectData()
+    {
+        driver = new ChromeDriver();
+        driver.get(url);
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+        WebElement button = driver.findElement(By.id("chat-widget-container"));
+
+        button.click();
+
+        WebElement appaltizer = driver.findElement(By.xpath("/html/body/main/article/div/ul[5]/li[1]"));
+
+        System.out.println(appaltizer.getText());
+       
+
+        List<WebElement> slopes = driver.findElements(By.className("slope-report__status-title"));
+
+        for(WebElement data : slopes)
+        {
+            System.out.println(data.getText());
+        }
+
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        driver.close();
+        driver.quit();
+        
+    }
 }
