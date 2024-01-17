baabdb01d60d437919f89554e129c861fa10f6b1
Jack <porterjp1@appstate.edu>
2024-01-17 17:10:34 -0500

    Create utility class for methods with shared functionality between classes

---
+++ src/main/java/shredding/club/snowcon/controller/Utility.java
@@ -0,0 +1,35 @@
+package shredding.club.snowcon.controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Utility 
+{
+
+    public static Parent root;
+    public static Stage stage;
+    public static Scene scene;
+    
+
+
+    public void switchScene(ActionEvent event, String scenePath)
+    {
+        try 
+        {
+            root = FXMLLoader.load(getClass().getResource(scenePath));
+            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) 
+        {
+            System.out.println("Incorrect path or file doesn't exist");
+        }
+    }
+}
