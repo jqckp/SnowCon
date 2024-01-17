71c1b29e93c6052081997dbbb60ee59654af5fa7
Jack <porterjp1@appstate.edu>
2024-01-17 16:46:37 -0500

    Create controllers for slope condition views

---
+++ src/main/java/shredding/club/snowcon/controller/BeechMountainController.java
@@ -0,0 +1,17 @@
+package shredding.club.snowcon.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class BeechMountainController implements Initializable
+{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) 
+    {
+        
+    }
+    
+}
