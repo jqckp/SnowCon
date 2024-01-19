451779dda2f161004ae8ed14b22fa08865c573a3
Jack <porterjp1@appstate.edu>
2024-01-19 10:24:48 -0500

    Add uniform functionality to go back to dashboard

--- src/main/java/shredding/club/snowcon/controller/BeechMountainController.java
+++ src/main/java/shredding/club/snowcon/controller/BeechMountainController.java
@@ -3,15 +3,32 @@ package shredding.club.snowcon.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 public class BeechMountainController implements Initializable
 {
+
+    @FXML
+    private Button backToWeather;
+
+    private Utility util;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) 
     {
         
+    }
+
+    @FXML
+    private void back(ActionEvent event)
+    {
+        util = new Utility();
+
+        util.switchScene(event, "../view/Dashboard.fxml");
+
     }
     
 }
--- src/main/java/shredding/club/snowcon/controller/SugarMountainController.java
+++ src/main/java/shredding/club/snowcon/controller/SugarMountainController.java
@@ -3,15 +3,30 @@ package shredding.club.snowcon.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 public class SugarMountainController implements Initializable
 {
+
+    @FXML
+    private Button backToWeather;
+
+    private Utility util;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) 
     {
         
+    }
+
+    @FXML
+    private void back(ActionEvent event)
+    {
+        util = new Utility();
+        util.switchScene(event, "../view/Dashboard.fxml");
     }
     
 }
