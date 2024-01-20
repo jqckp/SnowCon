dc5a9e6b543d2ecde5979ee25ef4bcbdf40d6203
Jack <porterjp1@appstate.edu>
2024-01-19 19:25:12 -0500

    Add relevant fields

--- src/main/java/shredding/club/snowcon/model/Unit.java
+++ src/main/java/shredding/club/snowcon/model/Unit.java
@@ -3,15 +3,35 @@ package shredding.club.snowcon.model;
 public enum Unit 
 {
     
-    METRIC('M'),
-    AMERICAN('I'),
-    SCIENTIFIC('S');
+    METRIC('M', 'C', "m/s", "mm", "km"),
+    AMERICAN('I', 'F', "mph", "in", "mi"),
+    SCIENTIFIC('S', 'K', "m/s", "mm", "km");
 
-    final char symbol;
+    public final char symbol;
+    public final char temperatureUnit;
+    public final String speedUnit;
+    public final String precipitationUnit;
+    public final String distanceUnit;
 
-
-    private Unit(char symbol)
+    
+    private Unit
+    (
+        char symbol,
+        char temperatureUnit,
+        String speedUnit,
+        String precipitationUnit,
+        String distanceUnit
+    )
     {
         this.symbol = symbol;
+        this.temperatureUnit = temperatureUnit;
+        this.speedUnit = speedUnit;
+        this.precipitationUnit = precipitationUnit;
+        this.distanceUnit = distanceUnit;
     }
+
+    
+
+
+
 }
