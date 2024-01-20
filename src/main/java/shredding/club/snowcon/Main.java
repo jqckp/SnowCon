e3e5b10490db149019353a3333d60ca71e74d035
Jack <porterjp1@appstate.edu>
2024-01-20 11:08:38 -0500

    Refactor

--- src/main/java/shredding/club/snowcon/Main.java
+++ src/main/java/shredding/club/snowcon/Main.java
@@ -1,15 +1,13 @@
 package shredding.club.snowcon;
 
-import shredding.club.snowcon.model.AppSkiMountain;
+
 import shredding.club.snowcon.view.GraphicalInterface;
 
-public class Main {
+public class Main 
+{
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         GraphicalInterface.launchGUI(args);
-
-        //AppSkiMountain appSki = new AppSkiMountain();
-
-        //appSki.collectData();
     }
 }
