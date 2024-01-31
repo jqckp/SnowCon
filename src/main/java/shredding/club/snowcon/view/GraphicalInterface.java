d6b65e1dc9e2bea38d74c8df88f1097db5542e3f
Jack <porterjp1@appstate.edu>
2024-01-31 08:53:49 -0500

    Load font

--- src/main/java/shredding/club/snowcon/view/GraphicalInterface.java
+++ src/main/java/shredding/club/snowcon/view/GraphicalInterface.java
@@ -6,9 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
-
-
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -29,7 +27,9 @@ public class GraphicalInterface extends Application
         String path = "./Dashboard.fxml";
 
         Parent root = FXMLLoader.load(getClass().getResource(path));
-            
+
+        Font.loadFont(getClass().getResourceAsStream("fonts/Snowy Season.ttf"), 12);
+        
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
