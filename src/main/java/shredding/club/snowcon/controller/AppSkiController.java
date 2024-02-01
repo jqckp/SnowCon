1adcb9278cf69c490e689323f4a5f877d56184bf
Jack <porterjp1@appstate.edu>
2024-01-31 19:45:39 -0500

    Add labels to indicate open/closed slopes

--- src/main/java/shredding/club/snowcon/controller/AppSkiController.java
+++ src/main/java/shredding/club/snowcon/controller/AppSkiController.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 
 public class AppSkiController implements Initializable
@@ -14,6 +15,49 @@ public class AppSkiController implements Initializable
 
     @FXML
     private Button backToOverview;
+
+    @FXML
+    private Label appaltizerStatus;
+
+    @FXML
+    private Label averysAppalStatus;
+
+    @FXML
+    private Label candiedAppalStatus;
+
+    @FXML
+    private Label strudelStatus;
+
+    @FXML
+    private Label orchardRunStatus;
+
+    @FXML
+    private Label lowerBigAppalStatus;
+
+    @FXML
+    private Label upperBigAppalStatus;
+
+    @FXML
+    private Label hardCoreStatus;
+
+    @FXML
+    private Label thinSliceStatus;
+
+    @FXML
+    private Label appaljackStatus;
+
+    @FXML
+    private Label appalJamStatus;
+
+    @FXML
+    private Label appalTopStatus;
+
+    @FXML
+    private Label theHighlineStatus;
+
+
+
+
 
     private Utility util;
     
